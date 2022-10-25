74487c140259115a04d211e811d01aecf1af1dd5
turalismylv <turalhi@code.edu.az>
2022-10-25 19:10:48 +0400

    HomeWork2510

---
+++ HomeWork2410/front-to-back/front-to-back/Controllers/WorkController.cs
@@ -0,0 +1,33 @@
+﻿using front_to_back.DAL;
+using front_to_back.ViewModels.Work;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace front_to_back.Controllers
+{
+    public class WorkController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public WorkController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            var categories = await _appDbContext.Categories
+                                                    .Include(c => c.CategoryComponents)
+                                                    .ToListAsync();
+
+
+            var model = new WorkIndexViewModel
+            {
+                Categories = categories
+            };
+
+
+            return View(model);
+        }
+    }
+}
---
+++ HomeWork2410/front-to-back/front-to-back/ViewModels/Work/WorkIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using front_to_back.Models;
+
+namespace front_to_back.ViewModels.Work
+{
+    public class WorkIndexViewModel
+    {
+        public List<Category> Categories { get; set; }
+    }
+}
